c4b9135d1f16d3f28d668de9162a329cd756b163
liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>
2020-05-30 19:41:05 +0800

    42

---
+++ components/my-cpn/my-cpn.js
@@ -0,0 +1,29 @@
+// components/my-cpn/my-cpn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // title : String
+    title :{
+      type : String,
+      value : "默认标题"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleInc(){
+      this.triggerEvent("increment",{},{})
+    }
+  }
+})
