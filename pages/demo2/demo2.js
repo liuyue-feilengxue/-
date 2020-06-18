9f1a5880d089d530cd44074531df346530cbbca5
liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>
2020-06-18 21:05:57 +0800

    pages change

---
+++ pages/demo2/demo2.js
@@ -0,0 +1,76 @@
+// pages/demo2/demo2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      name:options.name,
+      age:options.age
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    //1.获取首页的页面对象
+    const pages = getCurrentPages()
+    const demo1 = pages[pages.length - 2]
+
+    //2. 调用页面对象的setData
+    demo1.setData({
+      name : "I'm find thank you"
+    })
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
